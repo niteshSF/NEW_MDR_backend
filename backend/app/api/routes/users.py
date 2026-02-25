676cc29877cef3ca1caedc7d0f495f13fa0f6a3c
ajaynarayana <ajay.narayana@gmail.com>
2026-02-25 06:37:14 +0530

    removed items as it's not needed

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -13,7 +13,7 @@ from app.api.deps import (
 from app.core.config import settings
 from app.core.security import get_password_hash, verify_password
 from app.models import (
-    Item,
+    # Item,
     Message,
     UpdatePassword,
     User,
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from pydantic import EmailStr
+from typing import Optional
 from sqlmodel import Field, Relationship, SQLModel
 
 
@@ -41,10 +42,8 @@ class UpdatePassword(SQLModel):
 
 # Database model, database table inferred from class name
 class User(UserBase, table=True):
-    id: str = Field(default_factory=uuid.uuid4, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     hashed_password: str
-    items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
-
 
 # Properties to return via API, id is always required
 class UserPublic(UserBase):
@@ -55,41 +54,9 @@ class UsersPublic(SQLModel):
     data: list[UserPublic]
     count: int
 
-
-# Shared properties
-class ItemBase(SQLModel):
-    title: str = Field(min_length=1, max_length=255)
-    description: str | None = Field(default=None, max_length=255)
-
-
-# Properties to receive on item creation
-class ItemCreate(ItemBase):
-    pass
-
-
-# Properties to receive on item update
-class ItemUpdate(ItemBase):
-    title: str | None = Field(default=None, min_length=1, max_length=255)  # type: ignore
-
-
-# Database model, database table inferred from class name
-class Item(ItemBase, table=True):
-    id: str = Field(default_factory=uuid.uuid4, primary_key=True)
-    owner_id: uuid.UUID = Field(
-        foreign_key="user.id", nullable=False, ondelete="CASCADE"
-    )
-    owner: User | None = Relationship(back_populates="items")
-
-
-# Properties to return via API, id is always required
-class ItemPublic(ItemBase):
-    id: uuid.UUID
-    owner_id: uuid.UUID
-
-
-class ItemsPublic(SQLModel):
-    data: list[ItemPublic]
-    count: int
+#Shared properties
+class EntityBase(SQLModel):
+    id: Optional[int] = Field(default=None, primary_key=True)
 
 
 # Generic message
